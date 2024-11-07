ce2761a73dc9f326e079b3b61ca3d415908933cd
Dobby19 <southvi1@naver.com>
2024-11-07 15:23:35 +0900

    feat: 쿼리 기능

---
+++ src/main/java/umc/Spring/repository/StoreRepositoryImpl.java
@@ -0,0 +1,14 @@
+/*package umc.Spring.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import umc.Spring.domain.Store;
+import umc.Spring.domain.Qstore;
+
+
+
+public class StoreRepositoryImpl {
+}
+*/
